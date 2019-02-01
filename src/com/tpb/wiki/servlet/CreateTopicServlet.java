3cbe5a854bff63466fa08f2c14c29f6f9917e181
congptc <congptc@gmail.com>
2019-02-01 16:48:31 +0700

    Create CreateTopicServlet.java

---
+++ src/com/tpb/wiki/servlet/CreateTopicServlet.java
@@ -0,0 +1,31 @@
+package com.tpb.wiki.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CreateTopicServlet extends HttpServlet {
+
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, 
+			HttpServletResponse resp) throws ServletException, IOException {
+		RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/WEB-INF/views/createtopic.jsp");
+		dispatcher.forward(req, resp);
+		
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req,resp);
+	}
+}
