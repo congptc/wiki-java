b3a35068217abe0f887038a4b5f3c377d7cd8837
congptc <congptc@gmail.com>
2019-02-15 16:42:00 +0700

    Create UserBo.java

---
+++ src/com/tpb/wiki/bo/UserBo.java
@@ -0,0 +1,45 @@
+package com.tpb.wiki.bo;
+
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.sql.DataSource;
+
+import com.tpb.wiki.beans.User;
+import com.tpb.wiki.common.Constants;
+import com.tpb.wiki.common.Messages;
+import com.tpb.wiki.conn.DBCPDataSourceFactory;
+import com.tpb.wiki.da.UserDa;
+
+public class UserBo {
+	
+	private	UserDa _userDa = null;
+	
+	
+	public UserBo() {
+		DataSource ds = DBCPDataSourceFactory.getDataSource();
+		_userDa =  new UserDa(ds);
+	}
+	
+	public void login(HttpServletRequest req) {
+		
+		HttpSession session = req.getSession();
+		Messages message = null;
+		
+		String userName = req.getParameter("userName");
+		String password = req.getParameter("password");
+		
+		User user = _userDa.findUserByUserNameAndPassword(userName, password);
+		
+		if(user == null) {
+			message = new Messages(Constants.MessageType.ERROR, "User name or password wrong");
+		}else {
+			session.setAttribute(Constants.SESSION_USER_LOGIN, user);
+			message = new Messages(Constants.MessageType.SUCCESS, "Login success");
+		}
+		
+		req.setAttribute("message", message);
+		req.setAttribute("User", user);
+	}
+	
+}
