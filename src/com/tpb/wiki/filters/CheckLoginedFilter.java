9dd1d3a6e11f0460317111760af96ec9afc94cb5
congptc <congptc@gmail.com>
2019-02-15 14:46:38 +0700

    Create CheckLoginedFilter.java

---
+++ src/com/tpb/wiki/filters/CheckLoginedFilter.java
@@ -0,0 +1,48 @@
+package com.tpb.wiki.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.tpb.wiki.common.Constants;
+
+public class CheckLoginedFilter implements Filter {
+	 
+	  public CheckLoginedFilter() {
+	  }
+	 
+	  @Override
+	  public void init(FilterConfig fConfig) throws ServletException {
+	 
+	  }
+	 
+	  @Override
+	  public void destroy() {
+	 
+	  }
+	 
+	  @Override
+	  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+	          throws IOException, ServletException {
+		  HttpServletRequest req = (HttpServletRequest) request;
+		  HttpServletResponse res = (HttpServletResponse) response;
+		  HttpSession session = req.getSession();
+		  
+		  if(session.getAttribute(Constants.SESSION_USER_ID) == null) {
+			  RequestDispatcher dispatcher = req.getServletContext().getRequestDispatcher("/WEB-INF/views/login.jsp");
+				dispatcher.forward(req, res);
+		  }else {
+			  chain.doFilter(request, response);  
+		  }
+	  }
+
+}
