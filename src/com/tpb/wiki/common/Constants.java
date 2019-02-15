71b803441ee8eb41668eed64ddb0d102d1f3aa80
congptc <congptc@gmail.com>
2019-02-15 14:47:12 +0700

    Update Constants.java

--- src/com/tpb/wiki/common/Constants.java
+++ src/com/tpb/wiki/common/Constants.java
@@ -2,9 +2,13 @@ package com.tpb.wiki.common;
 
 public class Constants {
 	
+	public static final String SESSION_USER_ID = "USER_ID"; 
+	
 	public class MessageType{
 		public static final String SUCCESS = "SUCCESS";
 		public static final String ERROR = "ERROR";
 	}
+	
+
 
 }
